e76e14ca2eb72c96fb5ceafb29910504a71056cd
dog-broad <rushyendra.7004@gmail.com>
2023-07-05 13:55:24 +0000

    Jul 5, 2023, 7:25 PM

--- Easy_Pronunciation.c
+++ Easy_Pronunciation.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void){
-    int t;
-    scanf("%d", &t);
-    while(t--){
-        int n;
-        scanf("%d", &n);
-        char s[100];
-        scanf("%s", s);
-        int i;
-        // check if consecutive characters are consonants
-        int count = 0;
-        for(i=0; i<n-1; i++){
-            if(s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u'){
-                count++;
-            }
-            else{
-                count = 0;
-                continue;
-            }
-            if(count > 4){
-                printf("NO\n");
-                break;
-            }
+int main(void) {
+    char s[100];
+    scanf("%s", s);
+
+    int count = 0;
+    for (int i = 0; i < strlen(s); i++) {
+        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u') {
+            count++;
+        } else {
+            count = 0;
         }
-        if(count <= 4){
-            printf("YES\n");
+        if (count >= 4) {
+                printf("NO\n");
+                return 0;
         }
     }
+    printf("YES\n");
     return 0;
 }
