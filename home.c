eb471f24dca88d05acc8aad7d308a8a5bf3f9f4a
dog-broad <rushyendra.7004@gmail.com>
2023-07-06 10:36:19 +0530

    Improve Structure Array code

--- home.c
+++ home.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 
-struct CSD{ // Abstract Structure
+struct CSD{
     int id;
     char grade;
     double percentage;
 };
 
+struct CSD setStudent(int i, char g, double p){
+    struct CSD o;
+    o.id = i;
+    o.grade = g;
+    o.percentage = p;
+    return o;
+};
+
+void printStudent(struct CSD o){
+    printf("ID: %d\n", o.id);
+    printf("Grade: %c\n", o.grade);
+    printf("Percentage: %lf\n", o.percentage);
+}
+
 int main(){
-    struct CSD std1, std2;
-    std1.grade = 'A';
-    printf("Grade: %c\n", std1.grade);
+    int n;
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+    struct CSD std[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter %d student's ID, Grade, Percentage: ", i);
+        scanf("%d %c %lf", &std[i].id, &std[i].grade, &std[i].percentage);
+    }
+    puts("Student Details: \n");
+    for(int i = 0; i < n; i++){
+        printStudent(std[i]);
+        printf("\n");
+    }
     return 0;
 }
