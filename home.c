d38f5a24ac948039966ed93743be996c7dcb15f1
dog-broad <rushyendra.7004@gmail.com>
2023-08-21 09:53:36 +0000

    Aug 21, 2023, 3:23 PM

--- home.c
+++ home.c
@@ -1,10 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int top = -1;
+
+void push(char* stack, char c){
+    stack[++top] = c;
+}
+
+void pop(char* stack){
+    top--;
+}
+
 char* isBalanced(char* s) {
-    int i = 0, j = strlen(s) - 1;
-    while(i < j){
-        if(s[i] != s[j]) return "NO";
-        i++; j--;
+    int n = strlen(s);
+    // using stack to check if brackets are balanced
+    char stack[100000];
+    for(int i = 0; i < n/2; i++){
+        push(stack, s[i]);
     }
-    return "YES";
+    for(int i = n/2; i < n; i++){
+        if(stack[top] == '(' && s[i] == ')'){
+            pop(stack);
+        }
+        else if(stack[top] == '{' && s[i] == '}'){
+            pop(stack);
+        }
+        else if(stack[top] == '[' && s[i] == ']'){
+            pop(stack);
+        }
+        else{
+            return "NO";
+        }
+    }
 }
 
 int main(){
