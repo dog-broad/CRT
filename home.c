e325ae4f073b97bcf0d949c188306b375919d72d
dog-broad <rushyendra.7004@gmail.com>
2023-08-21 09:44:10 +0000

    Aug 21, 2023, 3:14 PM

--- home.c
+++ home.c
@@ -1,67 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct SLL{
-    int data; struct SLL *next;
-}*head = NULL, *tail = NULL;
-
-void push(int v){
-    struct SLL *n = malloc(sizeof(struct SLL));
-    n->data = v;
-    n->next = head;
-    if(head == NULL) 
-        tail = n;//if list is empty, firstNode is also tail
-    head = n;
+char* isBalanced(char* s) {
+    int i = 0, j = strlen(s) - 1;
+    while(i < j){
+        if(s[i] != s[j]) return "NO";
+        i++; j--;
+    }
+    return "YES";
 }
 
-void printSLL(struct SLL *p){
-    while(p!=NULL) { printf("%d ", p->data); p=p->next; }
-}
-void append(int v){
-  if(head == NULL) { push(v); return; }//1
-  struct SLL *n = malloc(sizeof(struct SLL)); n->data = v;
-  tail->next = n;
-  tail = n;
-}
-void delete(int v){
-    //Write Code Here!
-    struct SLL *p = head, *prev = NULL;
-    // If linked list is empty
-    if(p==NULL) 
-        return;
-    // Iterate over the list till we find the key to be deleted
-    while(p!=NULL && p->data!=v) {
-        prev = p;
-        p = p->next;
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        char s[100000];
+        scanf("%s", s);
+        printf("%s\n", isBalanced(s));
     }
-    // If key is present at head, move head to next node
-    if(prev==NULL) {
-        head = head->next;
-    }
-    // If key is present anywhere else
-    else 
-        prev->next = p->next;
-    // Free memory
-    free(p);
-}
-//DO NOT EDIT THE CODE BELOW IN ANYWAY!!
-int main()
-{
-    int n1, n2;scanf("%d%d",&n1, &n2);
-    while(n1--)
-    {
-        int k;
-        scanf("%d",&k);
-        push(k);x
-    }
-  	while(n2--)
-    {
-        int k;
-        scanf("%d",&k);
-        append(k);
-    }
-  int k;
-        scanf("%d",&k);
-        delete(k);
-    printSLL(head);
+    return 0;
 }
