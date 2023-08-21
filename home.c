ee9fb4ac915339846a0039518d5032d444db8e0e
dog-broad <rushyendra.7004@gmail.com>
2023-08-21 10:00:56 +0000

    Aug 21, 2023, 3:30 PM

--- home.c
+++ home.c
@@ -14,26 +14,28 @@ void pop(char* stack){
 
 char* isBalanced(char* s) {
     int n = strlen(s);
-    // using stack to check if brackets are balanced
-
     char stack[10000];
-    for(int i = 0; i < n/2; i++){
-        push(stack, s[i]);
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            push(stack, s[i]);
+        } else if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+            if (top < 0) {
+                return "NO";
+            }
+            char topChar = stack[top];
+            if ((topChar == '(' && s[i] == ')') ||
+                (topChar == '{' && s[i] == '}') ||
+                (topChar == '[' && s[i] == ']')) {
+                pop(stack);
+            } else {
+                return "NO";
+            }
+        }
     }
-
-    for(int i = n/2; i < n; i++){
-        if(stack[top] == '(' && s[i] == ')'){
-            pop(stack);
-        }
-        else if(stack[top] == '{' && s[i] == '}'){
-            pop(stack);
-        }
-        else if(stack[top] == '[' && s[i] == ']'){
-            pop(stack);
-        }
-        else{
-            return "NO";
-        }
+    if (top >= 0) {
+        return "NO";
+    } else {
+        return "YES";
     }
 }
 
